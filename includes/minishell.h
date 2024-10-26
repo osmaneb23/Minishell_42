f0ef6ec506d27b9536785ff90c78eef8146b36da
Osmane <osmane.sgb@gmail.com>
2024-10-27 01:26:14 +0200

    add comments and upgrade data

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 18:03:56 by obouayed          #+#    #+#             */
-/*   Updated: 2024/10/26 18:17:07 by obouayed         ###   ########.fr       */
+/*   Updated: 2024/10/27 01:23:56 by obouayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,14 @@
 # define CMD 6     // command
 # define ARG 7     // argument
 
+/*
+Structure to store the tokens:
+- value: value of the current token
+- type: type of the  current token (between 1 and 7)
+[1: INPUT, 2: HEREDOC, 3: TRUNC, 4: APPEND, 5: PIPE, 6: CMD, 7: ARG]
+- next: pointer to the next token
+- prev: pointer to the previous token
+*/
 typedef struct s_token
 {
 	char			*value;
@@ -52,13 +60,54 @@ typedef struct s_token
 
 }					t_token;
 
+/*
+Structure to store the data of minishell:
+- token: first token of the list
+- line: current line entered by the user
+- username: username of the user
+- exit_status: exit status of the last line
+*/
 typedef struct s_data
 {
 	t_token			*token;
+	char			*line;
 	char			*username;
+	int				exit_status;
 }					t_data;
 
-bool				cleanup(bool error, char *message, int ext);
+// Initialization functions
+
+void				initialize_data(t_data **data, char **env);
+void				assign_type_to_tokens(void);
+int					determine_basic_type(char *value);
+
+// Parsing functions
+
+int					check_misplacements(t_data *data);
+int					check_misplacements_pipe(t_token *token);
+int					check_misplacements_redirection(t_token *token);
+bool				openquote(char *line);
+
+// Tokenization functions
+
+bool				tokenization(char *line);
+char				*tokenizer(char *line, unsigned int *i, bool *squote_open,
+						bool *dquote_open);
+char				*handle_quote(char *line, unsigned int *i,
+						bool *squote_open, bool *dquote_open);
+bool				create_token(char *value);
+bool				add_token_to_list(t_token *new, t_token *token);
+
+// Utils functions
+
 void				*get_data(void);
+void				printf_tokens(t_data *data);
+t_token				*last_token(t_token *token);
+
+// Cleaning functions
+
+int					cleanup(int exit_status, char *message, int exit_call,
+						int fd);
+void				free_data(t_data **data);
 
 #endif
