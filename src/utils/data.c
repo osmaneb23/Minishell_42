4a6b704594a1ec66acd0d1e1aae3fca715996710
Osmane <osmane.sgb@gmail.com>
2024-10-27 03:38:46 +0100

    init env in data

--- src/utils/data.c
+++ src/utils/data.c
@@ -6,7 +6,7 @@
 /*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 00:55:40 by obouayed          #+#    #+#             */
-/*   Updated: 2024/10/27 02:59:33 by obouayed         ###   ########.fr       */
+/*   Updated: 2024/10/27 03:36:11 by obouayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	initialize_data(t_data **data, char **env)
 	(*data)->token = NULL;
 	(*data)->line = NULL;
 	(*data)->username = getenv("USER");
+	(*data)->env = env;
 	(*data)->exit_status = 0;
 }
