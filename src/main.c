339fa7e32ddaf51f94d065e7dfe9ed1fbb0e91fc
Osmane <osmane.sgb@gmail.com>
2024-10-27 02:50:11 +0100

    add check valid comments and clean tokens

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 18:57:23 by obouayed          #+#    #+#             */
-/*   Updated: 2024/10/27 01:21:20 by obouayed         ###   ########.fr       */
+/*   Updated: 2024/10/27 02:42:33 by obouayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int	main(int ac, char **av, char **env)
 {
 	t_data	*data;
+
+	data = malloc(sizeof(t_data));
 
 	(void)ac;
 	(void)av;
@@ -29,8 +31,10 @@ int	main(int ac, char **av, char **env)
 			tokenization(data->line);
 			printf_tokens(data);
 			check_misplacements(data);
-			while (data->token)
-				data->token = data->token->next;
+			if (check_valid_commands(data))
+				cleanup(127, NULL, NO_EXIT, 2);
+			else
+				free_tokens(&data);	
 		}
 	}
 	return (cleanup(SUCCESS, NULL, NO_EXIT, 1));
