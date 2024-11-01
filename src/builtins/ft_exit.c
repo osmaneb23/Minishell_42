8a2c029402a4afad49ca3ffc3520e8f76ed804a2
Osmane <osmane.sgb@gmail.com>
2024-11-01 23:31:20 +0100

    add buitlins files

---
+++ src/builtins/ft_exit.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 23:27:50 by obouayed          #+#    #+#             */
+/*   Updated: 2024/11/01 23:28:55 by obouayed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int ft_exit(char **args)
+{
+	int i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	if (i > 2)
+		return (cleanup(2, "exit: too many arguments\n", 1, 2));
+	if (i == 1)
+		exit(0);
+	if (!ft_isdigit(args[1][0]))
+		return (cleanup(2, "exit: numeric argument required\n", 255, 2));
+	exit(ft_atoi(args[1]));
+	return (SUCCESS);
+}
