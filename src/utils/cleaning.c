65cafe9c700107e203a89af6ea3ebfdefc117824
Osmane <osmane.sgb@gmail.com>
2024-11-02 19:08:15 +0100

    add free commands and fix norm

--- src/utils/cleaning.c
+++ src/utils/cleaning.c
@@ -6,7 +6,7 @@
 /*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 02:27:41 by obouayed          #+#    #+#             */
-/*   Updated: 2024/11/02 18:01:32 by obouayed         ###   ########.fr       */
+/*   Updated: 2024/11/02 19:07:05 by obouayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,11 @@ int	cleanup(int exit_status, char *message, int exit_call, int fd)
 	t_data	*data;
 
 	data = get_data();
-	free_data(&data);
-	data->exit_status = exit_status;
+	if (data)
+	{
+		free_data(&data);
+		data->exit_status = exit_status;
+	}
 	if (message)
 		ft_putstr_fd(message, fd);
 	if (exit_call != NO_EXIT)
@@ -39,8 +42,10 @@ void	free_data(t_data **data)
 {
 	if (!data || !(*data))
 		return ;
-	free_tokens(data);
-	free_commands(data);
+	if ((*data)->token)
+		free_tokens(data);
+	if ((*data)->cmd)
+		free_commands(data);
 }
 
 // Free the tokens in the list
@@ -58,6 +63,7 @@ void	free_tokens(t_data **data)
 		if (current->value)
 			free(current->value);
 		free(current);
+		current = NULL;
 		current = next;
 	}
 	if ((*data)->token)
@@ -68,7 +74,7 @@ void	free_commands(t_data **data)
 {
 	t_cmd	*current;
 	t_cmd	*next;
-	unsigned int		i;
+	int		i;
 
 	if (!data || !*data || !(*data)->cmd)
 		return ;
@@ -79,16 +85,16 @@ void	free_commands(t_data **data)
 		if (current->cmd_param)
 		{
 			i = 0;
-			while (current->cmd_param[i])
-			{
+			if (current->cmd_param[i])
 				free(current->cmd_param[i]);
-				i++;
-			}
+			while (current->cmd_param[i] && ++i)
+				free(current->cmd_param[i]);
 			free(current->cmd_param);
+			current->cmd_param = NULL;
 		}
 		free(current);
+		current = NULL;
 		current = next;
 	}
-	if ((*data)->cmd)
-		(*data)->cmd = NULL;
+	(*data)->cmd = NULL;
 }
