e21f63da1f6a4c1c71ed44a4ba713c86fb70c04f
Osmane <osmane.sgb@gmail.com>
2024-11-02 23:51:07 +0100

    replace value with variable

--- src/main.c
+++ src/main.c
@@ -6,59 +6,34 @@
 /*   By: obouayed <obouayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 18:57:23 by obouayed          #+#    #+#             */
-/*   Updated: 2024/11/02 22:55:54 by obouayed         ###   ########.fr       */
+/*   Updated: 2024/11/02 23:49:43 by obouayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-char	*search_env_var(char *var)
+void	replace_value_with_variable(t_data *data)
 {
-	int		len;
 	char	*value;
-	t_env	*tmp;
-	t_data	*data;
-
-	if (!var)
-		return (NULL);
-	data = get_data();
-	if (!data || !data->envp)
-		return (NULL);
-	len = ft_strlen(var);
-	tmp = data->envp;
-	while (tmp != NULL)
-	{
-		if (ft_strncmp(var, tmp->line, len) == 0 && tmp->line[len] == '=')
-		{
-			value = malloc(ft_strlen(tmp->line + len + 1) + 1);
-			if (!value)
-				return (NULL);
-			ft_strcpy(value, tmp->line + len + 1);
-			return (value);
-		}
-		tmp = tmp->next;
-	}
-	return (NULL);
-}
-void replace_value_with_variable(t_data *data)
-{
-	char    *value;
-	char    *tmp;
-	char *tmp2;
-	t_token *token;
+	char	*tmp;
+	char	*tmp2;
+	t_token	*token;
 
 	token = data->token;
 	while (token)
 	{
-		if ((token->value[0] == '$') || (token->value[0] == '"' && token->value[1] == '$'))
+		if ((token->value[0] == '$') || (token->value[0] == '"'
+				&& token->value[1] == '$'))
 		{
-			if (token->value[0] == '"')
-				remove_quotes(data);
 			tmp2 = token->value;
-			token->value = ft_substr(token->value, 1, ft_strlen(token->value) - 1);
+			if (token->value[0] == '"')
+				token->value = ft_substr(token->value, 2,
+					ft_strlen(token->value) - 2);
+			else
+				token->value = ft_substr(token->value, 1,
+					ft_strlen(token->value) - 1);
 			free(tmp2);
 			value = search_env_var(token->value);
-			printf("value: %s\n", value);
 			if (value)
 			{
 				tmp = token->value;
@@ -70,11 +45,14 @@ void replace_value_with_variable(t_data *data)
 	}
 }
 
-bool	main_routine(t_data *data)
+bool	main_routine(t_data *data, char **envp)
 {
+	(void)envp;
 	if (!check_openquote(data->line))
 	{
+		// Echo ""$USER""
 		tokenization(data->line);
+		init_environment(data, envp);
 		replace_value_with_variable(data);
 		remove_quotes(data);
 		init_cmd(data);
@@ -88,13 +66,13 @@ bool	main_routine(t_data *data)
 	return (SUCCESS);
 }
 
-int	main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **envp)
 {
 	t_data	*data;
 
 	(void)ac;
 	(void)av;
-	initialize_data(&data, env);
+	initialize_data(&data);
 	setup_signals();
 	while (1)
 	{
@@ -104,7 +82,7 @@ int	main(int ac, char **av, char **env)
 			rl_clear_history();
 			return (cleanup(SUCCESS, "exit\n", NO_EXIT, 2));
 		}
-		main_routine(data);
+		main_routine(data, envp);
 		add_history(data->line);
 	}
 	rl_clear_history();
